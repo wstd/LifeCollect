c6bce84517c0363749b9d7bdf1b5c4a95152b68c
Roman Rogov <roman.r@aggrostudios.com>
2015-09-22 20:52:10 +0300

    navmesh guys

---
+++ Assets/AITest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AITest : MonoBehaviour
+{
+	public List<Transform> NavPoints;
+
+	private NavMeshAgent agent;
+	private int pointIndex = 0;
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+		agent.destination = NavPoints[pointIndex].position;
+	}
+
+	void Update ()
+	{
+		Vector2 myPos = new Vector2(transform.position.x, transform.position.z);
+		Vector2 pointPos = new Vector2(NavPoints[pointIndex].position.x, NavPoints[pointIndex].position.z);
+
+		if (Vector2.Distance(myPos, pointPos) < .1f)
+		{
+			pointIndex = (pointIndex == NavPoints.Count-1) ? 0 : (pointIndex + 1);
+			agent.destination = NavPoints[pointIndex].position;
+		}
+	}
+}
---
+++ Assets/MeshNoiser.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshNoiser : MonoBehaviour
+{
+
+	private Mesh mesh;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//StartCoroutine(Renoise());
+	}
+
+	private void Update()
+	{
+		transform.Rotate(Vector3.up);
+	}
+	
+	private IEnumerator Renoise()
+	{
+		while (true)
+		{
+			mesh = GetComponent<MeshFilter>().mesh;
+			Vector3[] vertices = mesh.vertices;
+			int p = 0;
+			while (p < vertices.Length)
+			{
+				vertices[p].y = Random.Range(-.3f, .3f);
+				p++;
+			}
+			mesh.vertices = vertices;
+			mesh.RecalculateNormals();
+			yield return new WaitForSeconds(Random.Range(.05f, .3f));
+		}
+	}
+}
